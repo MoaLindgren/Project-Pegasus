9536efd23eacb05d8694233bb6431862f558844a
Satkaaktas <Timmy_nitr0@hotmail.com>
2019-08-11 15:53:51 +0200

    PlayerComponent

---
+++ GrannyWars/Assets/Scripts/H_PlayerMovement.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class H_PlayerMovement : MonoBehaviour
+{
+	C_Player[] players;
+
+	public H_PlayerMovement(C_Player[] playerComponents)
+	{
+		this.players = playerComponents;
+	}
+
+	public void Tick()
+	{
+		float _horizontalAxis = Input.GetAxis("Horizontal");
+		float _verticalAxis = Input.GetAxis("Vertical");
+	}
+}
