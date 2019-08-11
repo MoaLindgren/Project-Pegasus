063f5c4eb541f499b6a6dd9db9378f4ae3e89e2d
Satkaaktas <Timmy_nitr0@hotmail.com>
2019-08-11 17:22:31 +0200

    Buggy rotation

--- GrannyWars/Assets/Scripts/H_PlayerMovement.cs
+++ GrannyWars/Assets/Scripts/H_PlayerMovement.cs
@@ -19,10 +19,25 @@ public class H_PlayerMovement: HandlerBehaviour
 		{
 			float _horizontalAxis = Input.GetAxis("Horizontal");
 			float _verticalAxis = Input.GetAxis("Vertical");
+			Vector3 _previousPosition = p.transform.position;
+			
+			//Calculate position
+			Vector3 _position = _previousPosition + (Vector3.forward * _verticalAxis + Vector3.right * _horizontalAxis) * players[0].speed * _deltaTime;
+			p.transform.position = _position;
 
-			p.transform.position += (Vector3.forward * _verticalAxis + Vector3.right * _horizontalAxis) * players[0].speed * _deltaTime;
-
+			//Calculate rotation
+			Vector3 _direction = _position - _previousPosition;
+			_direction = _direction - Vector3.up * _direction.y;
+			p.transform.Rotate(Vector3.up, GetRotation(p.transform.forward, _direction));
 		}
 
+	}
+
+	private float GetRotation(Vector3 from, Vector3 to)
+	{
+		Vector2 f = new Vector2(from.x, from.z);
+		Vector2 t = new Vector2(to.x, to.z);
+		return Vector2.Angle(f,t);
+		
 	}
 }
