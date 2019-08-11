f1b3cc9aafcffdf524168a77ca1394823ab30094
MoaLindgren <moa.lindgren@outlook.com>
2019-08-11 16:08:34 +0200

    playercomponent changes

--- GrannyWars/Assets/Scripts/C_Player.cs
+++ GrannyWars/Assets/Scripts/C_Player.cs
@@ -4,5 +4,13 @@ using UnityEngine;
 
 public class C_Player : MonoBehaviour
 {
-	[SerializeField] float speed;
+    [Header("Movement Settings")]
+    public float speed;
+
+    [Header("Damage Settings")]
+    public float basicAttackDamage;
+    public float basicAttackSpeed;
+    public float[] abilityAttackDamage;
+    public float[] abilityAttackSpeed;
+    public float[] abilityCooldown;
 }
