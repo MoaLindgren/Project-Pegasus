8c0522b0f031373cdfd445e0a95767c3276d3d7e
Satkaaktas <Timmy_nitr0@hotmail.com>
2019-08-11 15:33:59 +0200

    Jag vill göra en full commit

--- GrannyWars/Assets/Scripts/C_Player.cs
+++ GrannyWars/Assets/Scripts/C_Player.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EntryPoint : MonoBehaviour
+public class C_Player : MonoBehaviour
 {
-    
+	[SerializeField] float speed;
 }
---
+++ GrannyWars/Assets/Scripts/H_Movement.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class H_Movement : MonoBehaviour
+{
+	C_Player player;
+
+	public H_Movement(C_Player playerComponent)
+	{
+		this.player = playerComponent;
+	}
+}
