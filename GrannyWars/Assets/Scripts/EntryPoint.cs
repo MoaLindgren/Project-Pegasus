fba9b47d7739d267c12992049a9455b01fae7295
Satkaaktas <Timmy_nitr0@hotmail.com>
2019-09-08 14:11:07 +0200

    EntryPoint changes
    Enemy support

--- GrannyWars/Assets/Scripts/EntryPoint.cs
+++ GrannyWars/Assets/Scripts/EntryPoint.cs
@@ -5,17 +5,25 @@ using UnityEngine;
 public class EntryPoint : MonoBehaviour
 {
 	[SerializeField] C_Player[] players;
+	[SerializeField] C_Enemy[] enemies;
 
 	H_PlayerMovement movement;
+	H_EnemyMovement enemyMovement;
 	private void Start()
 	{
 		movement = new H_PlayerMovement(players);
 		movement.Start();
+
+		enemyMovement = new H_EnemyMovement(enemies);
+		enemyMovement.Start();
+
+
 	}
 
 	private void Update()
 	{
 		movement.Tick();
+		enemyMovement.Tick();
 	}
 }
 
