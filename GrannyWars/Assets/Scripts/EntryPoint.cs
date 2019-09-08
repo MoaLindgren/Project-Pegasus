1829e0420bc6e8726b95fdf77b930d58d9c62df8
MoaLindgren <moa.lindgren@outlook.com>
2019-09-08 16:58:27 +0200

    Update EntryPoint.cs

--- GrannyWars/Assets/Scripts/EntryPoint.cs
+++ GrannyWars/Assets/Scripts/EntryPoint.cs
@@ -7,20 +7,19 @@ public class EntryPoint : MonoBehaviour
 	[SerializeField] private C_Player[] players;
 	[SerializeField] private C_Enemy[] enemies;
 	[SerializeField] private C_Obsticle[] obsticles;
-    [SerializeField] private GameObject objectPool;
-    [SerializeField] private int[] nbrOfObjects;
+
+    [Header("Objectpool")]
+    [SerializeField] private C_Projectile[] projectiles = new C_Projectile[20];
 
     private H_PlayerMovement movement;
     private H_EnemyMovement enemyMovement;
     private H_PlayerAttacking attacking;
 	private H_Obsticle obsticle;
 
-
-
     private void Start()
 	{
 		movement = new H_PlayerMovement(players);
-        attacking = new H_PlayerAttacking(players[0]);
+        attacking = new H_PlayerAttacking(players[0], projectiles);
 		obsticle = new H_Obsticle(obsticles);
 		enemyMovement = new H_EnemyMovement(enemies);
 	}
