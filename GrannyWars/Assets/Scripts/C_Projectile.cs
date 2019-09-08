b78496078495c7faa3b74620091efe6cf4aaa3be
MoaLindgren <moa.lindgren@outlook.com>
2019-09-08 16:59:04 +0200

    Create C_Projectile.cs

---
+++ GrannyWars/Assets/Scripts/C_Projectile.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class C_Projectile : MonoBehaviour
+{
+    public float speed;
+    public float damage;
+}
